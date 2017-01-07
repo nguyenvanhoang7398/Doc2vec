eb0db5fede6df74f5b908e27222a316a26df1c9d
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2017-01-07 15:14:41 +0800

    Update pseudo code

--- doc2vec.py
+++ doc2vec.py
@@ -9,9 +9,57 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from preprocessing_data import load_data, time_format, custom_tokenizer, process_new_docs
 
 class doc2vec(BaseEstimator, TransformerMixin):
+
+    '''
+    Initialize a model from an existing 'doc_dataset_pickle' created by 'build_doc_dataset' function
+    in preprocessing_data.py
+
+    'batch_size' is the number of feature-label set in each training batch. By default, batch_size = 128
+
+    'window_size' is the number of consecutive words used in DM model to predict the next word
+
+    'concat' defines whether we concat the vectors of words in each training input (DM-concat model)
+    or simply average them (DM no-concat model)
+
+    'doc_embedding_size' is the dimensionality of feature vector for document
+
+    'word_embedding_size' is the dimensionality of feature vector for word
+
+    'vocab_size' is the number of unique word in vocabulary
+
+    'document_size' is the number of document used to train
+
+    'n_neg_samples' is the number of negative sample used in sampling
+
+    'learning_rate' is the learning rate used in optimization part. By default, learning_rate = 0.025
+
+    'doc_dataset.pickle' is the name of docs data set file (pickle) in the same directory. By default,
+    doc_dataset_pickle = 'docs_dataset.pickle'
+
+    'loss_type' defines the type of loss, either 'sampled_softmax_loss for softmax loss or
+    'nce_loss' for 'noise contrastive estimation' loss. By default, 'loss_type' = 'nce_loss'
+
+    'optimize' defines the type of optimizer, either 'Adagrad' for AdagradOptimizer or 'GradDes'
+    for GradientDescentOptimizer. By default, 'optimize' = 'Adagrad'
+
+    'model' defines the model of Doc2vec, either 'pv-dm' for Paragraph Vector - Distributed Memory
+    or 'pv-dbow' for Paragraph Vector - Distributed Bags of Word. By default, 'model' = 'pv-dm'
+
+    'n_skip' is the number of context words chosen with each predicted word (n_skip <= 2 * skip_window). By
+    default, 'n_skip' = None
+
+    'skip_window' is the maximum distance between context words chosen and their predicted word
+
+    'log' is the name of log file to record the training. This file is saved in the same directory as
+    doc2vec.py. By default, 'log' = 'doc2vec_log.pickle'
+
+    'path' is the saving directory of the model. By default, path = '/doc2vec_model/'
+    '''
+
+
     def __init__(self, batch_size=128, window_size=8, concat=True, doc_embedding_size=128, word_embedding_size=128,
                  vocab_size=30000, document_size=50000, n_neg_samples=64,
-                 learning_rate=1.0, doc_dataset_pickle='docs_dataset.pickle',
+                 learning_rate=0.025, doc_dataset_pickle='docs_dataset.pickle',
                  loss_type='sampled_softmax_loss',
                  optimize='Adagrad',
                  model='pv-dm', n_skip=None,
@@ -21,6 +69,7 @@ class doc2vec(BaseEstimator, TransformerMixin):
 
         print('Creating new doc2vec instance')
 
+        # Initialize params
         self.batch_size = batch_size
         self.window_size = window_size
         self.concat = concat
@@ -38,64 +87,97 @@ class doc2vec(BaseEstimator, TransformerMixin):
         self.data_index = 0
         self.log = log
         self.path = path
+
+        # Get doc_idx, word_idx, count, dictionary, reverse_dictionary by processing doc data set file
         self.doc_idx, self.word_idx, self.count, self.dictionary, self.reverse_dictionary = load_data(
             doc_dataset_pickle)
 
+        # Initialize the chosen model
         self._init_model()
+
+        # Initialize graph according to chosen model
         self._init_graph()
 
+        # Create session based on created graph
         self.sess = tf.Session(graph=self.graph)
 
     def _init_model(self):
         if self.model == 'pv-dbow':
+
+            # PV-DBOW model chosen
             print('Initialize doc2vec pv-dbow model')
+
+            # Initialize more params
             self._init_graph = self._init_graph_dbow
             self.span = self.skip_window * 2 + 1
             self.generate_batch = self.generate_batch_dbow
             self.fit = self.fit_dbow
 
         elif self.model == 'pv-dm':
+
+            # PV-DM model chosen
             print('Initialize doc2vec pv-dm model')
 
+            # Initialize more params
             self._init_graph = self._init_graph_dm
             self.span = self.window_size + 1
             self.generate_batch = self.generate_batch_dm
             self.fit = self.fit_dm
 
+    # Instance method to generate the next batch of PV-DBOW model
     def generate_batch_dbow(self):
         assert self.batch_size % self.n_skip == 0
         assert self.n_skip <= 2 * self.skip_window
 
+        # Initialize numpy array of each variable
         batch = np.ndarray(shape=(self.batch_size, self.n_skip), dtype=np.int32)
         labels = np.ndarray(shape=(self.batch_size, 1), dtype=np.int32)
         doc_labels = np.ndarray(shape=(self.batch_size, 1), dtype=np.int32)
         is_final = False
 
+        # Buffers for reading frame of both word and document with max length = self.span
         buffer = deque(maxlen=self.span)
         buffer_doc = deque(maxlen=self.span)
 
+        # Fill buffer with word_idx and buffer_doc with doc_idx in range of self.span
         for _ in range(self.span):
             buffer.append(self.word_idx[self.data_index])
             buffer_doc.append(self.doc_idx[self.data_index])
+
+            # Check if we have reached the end of document. If so, signal the model by is_final variable
+            # to stop generating batch and end current epoch
             if self.data_index == (len(self.word_idx) - 1):
                 is_final = True
                 print('Final reached')
+
+            # Use modulo to avoid IndexOutOfBoundException
             self.data_index = (self.data_index + 1) % len(self.word_idx)
 
+        # Fill batch with train-inputs and labels
         i = 0
         while i < self.batch_size:
+
+            # Check if all elements in the current buffer_doc are in the same doc
             if len(set(buffer_doc)) == 1:
+
+                # Initialize the target at the center of the spanning frame
                 target = self.skip_window
                 targets_to_avoid = [self.skip_window]
+
+                # Create smaller batch
                 batch_temp = np.ndarray(shape=(self.n_skip), dtype=np.int32)
 
                 for j in range(self.n_skip):
                     while target in targets_to_avoid:
+
+                        # Randomly pick another target and append to 'targets ti avoid'
                         target = random.randint(0, self.span - 1)
                     targets_to_avoid.append(target)
                     batch_temp[j] = buffer[target]
 
                 batch[i] = batch_temp
+
+                # Label is the target word in the center of the spanning fram
                 labels[i, 0] = buffer[self.skip_window]
                 doc_labels[i, 0] = self.doc_idx[self.data_index]
                 i += 1
@@ -114,31 +196,37 @@ class doc2vec(BaseEstimator, TransformerMixin):
         self.graph = tf.Graph()
         with self.graph.as_default():
 
+            # Initialize tensorflow placeholder
             self.train_inputs = tf.placeholder(tf.int32, shape=[self.batch_size, self.n_skip])
             self.train_labels = tf.placeholder(tf.int32, shape=[self.batch_size, 1])
             self.train_doc_labels = tf.placeholder(tf.int32, shape=[self.batch_size, 1])
 
+            # Initialize word_embeddings with vectors generated by uniform random
             self.word_embeddings = tf.Variable(
                 tf.random_uniform([self.vocab_size, self.word_embedding_size], -1.0, 1.0))
 
             self.doc_embeddings = tf.Variable(
                 tf.random_uniform([self.document_size, self.doc_embedding_size], -1.0, 1.0))
 
+            # Use embedding_lookup Op from tensorflow
             self.word_embed = tf.nn.embedding_lookup(self.word_embeddings, self.train_inputs)
 
             self.doc_embed = tf.nn.embedding_lookup(self.doc_embeddings, self.train_doc_labels)
 
             print('Shapes:', self.word_embed.get_shape(), self.doc_embed.get_shape())
 
+            # Use concat Op from tensorflow to concat word_embedding vector and doc_embedding vector
             embed = tf.concat(1, [self.word_embed, self.doc_embed])
 
             reduced_embed = tf.div(tf.reduce_sum(embed, 1), self.span)
 
+            # Initialize variable of weights and biases
             self.weights = tf.Variable(
                 tf.truncated_normal([self.vocab_size, self.doc_embedding_size],
                                     stddev=1.0 / math.sqrt(self.doc_embedding_size)))
             self.biases = tf.Variable(tf.zeros([self.vocab_size]))
 
+            # Use either sampled_softmax_loss Op or nce_loss Op from tensorflow to find loss
             if self.loss_type == 'sampled_softmax_loss':
                 loss = tf.nn.sampled_softmax_loss(self.weights, self.biases, reduced_embed,
                                                   self.train_labels, self.n_neg_samples, self.vocab_size)
@@ -147,21 +235,28 @@ class doc2vec(BaseEstimator, TransformerMixin):
                                       self.train_labels, self.n_neg_samples, self.vocab_size)
             self.loss = tf.reduce_mean(loss)
 
+            # Use optimizer Op from tensorflow corresponding to params
             if self.optimize == 'Adagrad':
                 self.optimizer = tf.train.AdagradOptimizer(self.learning_rate).minimize(loss)
             elif self.optimize == 'GradDes':
                 global_step = tf.Variable(0, trainable=False)
+
+                # Use exponential_decay to stabilize learning rate
                 modified_learning_rate = tf.train.exponential_decay(self.learning_rate, global_step,
                                                                     1000, 0.009, staircase=True)
                 self.optimizer = tf.train.GradientDescentOptimizer(modified_learning_rate).minimize(loss)
 
+            # Normalize word_embeddings and doc_embeddings
             word_norm = tf.sqrt(tf.reduce_sum(tf.square(self.word_embeddings), 1, keep_dims=True))
             self.normalized_word_embeddings = self.word_embeddings / word_norm
 
             doc_norm = tf.sqrt(tf.reduce_sum(tf.square(self.doc_embeddings), 1, keep_dims=True))
             self.normalized_doc_embeddings = self.doc_embeddings / doc_norm
 
+            # Creat init_op to initalize all tensorflow variables
             self.init_op = tf.initialize_all_variables()
+
+            # Create saver to save the current session of instance
             self.saver = tf.train.Saver()
 
             print('Successfully _init_graph of pv-dbow model')
@@ -170,11 +265,17 @@ class doc2vec(BaseEstimator, TransformerMixin):
 
         print('Fitting pv-dbow model')
 
+        # Run init Op
         sess = self.sess
         sess.run(self.init_op)
 
+        # Try to open log if exists, otherwise start the training with epoch = 1 and
+        # count total number of batches('total_batches') from 0
         try:
             with open(self.log, 'rb') as f:
+
+                # Load number of trained epoch ('epoch') and number of total batches
+                # ('total_batches') from log
                 epoch, total_batches = pickle.load(f)
                 epoch += 1
             print('STARTING WITH EPOCH:', epoch)
@@ -182,33 +283,45 @@ class doc2vec(BaseEstimator, TransformerMixin):
             epoch = 1
             total_batches = 0
         while epoch <= n_epoch:
+
+            # Restore saved model if not first epoch
             if epoch != 1:
                 self.saver.restore(self.sess, self.path + "model.ckpt")
 
+            # Initialize variables for new epoch
             epoch_loss = 0
             batches_run = 0
             is_final = False
             time0 = datetime.now()
 
+            # Keep generating new batches while end of file hasn't been reached
             while not is_final:
 
+                # Get variables from generate_batch_dbow and feed into current session
                 batch_inputs, batch_labels, batch_doc_labels, is_final = self.generate_batch_dbow()
                 feed_dict = {self.train_inputs: batch_inputs, self.train_labels: batch_labels,
                              self.train_doc_labels: batch_doc_labels}
 
+                # Run session to get loss value ('loss_val')
                 _, loss_val = self.sess.run([self.optimizer, self.loss], feed_dict=feed_dict)
                 epoch_loss += loss_val
                 batches_run += 1
+
+                # Keep track of 'total_batches' by increment if first epoch
                 if epoch == 1:
                     total_batches += 1
 
+                # Check process after every 5000 batches_run
                 if batches_run % 5000 == 0:
+
+                    # Formatting elapsed time
                     elapsed = (datetime.now() - time0)
                     second = elapsed.total_seconds()
                     time = time_format(second)
                     time_per_batch = float(second) / 5000
                     time0 = datetime.now()
 
+                    # Total number of batches is not ready in the first epoch
                     if epoch != 1:
                         print('Batch run:', batches_run, '/', total_batches, '| Epoch:', epoch,
                               '| Average batch loss:', epoch_loss / (batches_run), '| Time:', time[0], 'hours',
@@ -227,14 +340,19 @@ class doc2vec(BaseEstimator, TransformerMixin):
 
             print('Epoch', epoch, 'completed out of', n_epoch, 'loss:', epoch_loss)
 
+            # Write to log file 'epoch' and 'total_batches'
             with open(self.log, 'wb') as f:
                 pickle.dump((epoch, total_batches), f)
             epoch += 1
 
+            # Reset data_index to 0
             self.data_index = 0
+
+            # Run session to get finalized_word_embeddings and finalized_docs_embeddings
             self.finalized_word_embeddings = self.sess.run(self.normalized_word_embeddings)
             self.finalized_doc_embeddings = self.sess.run(self.normalized_doc_embeddings)
 
+            # Save current session
             print('Saving current session')
             self.save()
             print('Session saved successfully')
@@ -250,6 +368,7 @@ class doc2vec(BaseEstimator, TransformerMixin):
 
         buffer = deque(maxlen=self.span)
         buffer_doc = deque(maxlen=self.span)
+
 
         for _ in range(self.span):
             buffer.append(self.word_idx[self.data_index])
@@ -284,9 +403,11 @@ class doc2vec(BaseEstimator, TransformerMixin):
         self.graph = tf.Graph()
         with self.graph.as_default():
 
+            # Initialize tensorflow placeholder
             self.train_inputs = tf.placeholder(tf.int32, shape=[self.batch_size, self.window_size + 1])
             self.train_labels = tf.placeholder(tf.int32, shape=[self.batch_size, 1])
 
+            # Initialize word_embeddings with vectors generated by uniform random
             self.word_embeddings = tf.Variable(
                 tf.random_uniform([self.vocab_size, self.word_embedding_size], -1.0, 1.0))
 
@@ -304,12 +425,15 @@ class doc2vec(BaseEstimator, TransformerMixin):
             self.biases = tf.Variable(tf.zeros([self.vocab_size]))
 
             embed = []
+
+            # Use embedding_lookup Op from tensorflow
             if self.concat:
+                # If concat then append every word vector to embed
                 for j in range(self.window_size):
                     word_embed = tf.nn.embedding_lookup(self.word_embeddings, self.train_inputs[:, j])
                     embed.append(word_embed)
             else:
-                # averaging word vectors
+                # else average word vectors
                 word_embed = tf.zeros([self.batch_size, self.word_embedding_size])
                 for j in range(self.window_size):
                     word_embed += tf.nn.embedding_lookup(self.word_embeddings, self.train_inputs[:, j])
@@ -320,6 +444,7 @@ class doc2vec(BaseEstimator, TransformerMixin):
 
             self.embed = tf.concat(1, embed)
 
+            # Use either sampled_softmax_loss Op or nce_loss Op from tensorflow to find loss
             if self.loss_type == 'sampled_softmax_loss':
                 loss = tf.nn.sampled_softmax_loss(self.weights, self.biases, self.embed,
                                                   self.train_labels, self.n_neg_samples, self.vocab_size)
@@ -331,6 +456,7 @@ class doc2vec(BaseEstimator, TransformerMixin):
             if self.optimize == 'Adagrad':
                 self.optimizer = tf.train.AdagradOptimizer(self.learning_rate).minimize(loss)
             elif self.optimize == 'GradDes':
+                # Use exponential_decay to stabilize learning rate
                 self.optimizer = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(loss)
 
             word_norm = tf.sqrt(tf.reduce_sum(tf.square(self.word_embeddings), 1, keep_dims=True))
@@ -499,7 +625,7 @@ class doc2vec(BaseEstimator, TransformerMixin):
                 batches_run += 1
 
             print('Epoch', epoch, 'completed out of', n_epoch, 'loss:', epoch_loss, 'batches_run:', batches_run,
-                  'loss per batch: %.2f' % float(loss_val/batches_run))
+                  'loss per batch: %.2f' % float(epoch_loss/batches_run))
             epoch += 1
 
             self.new_data_index = 0
--- use_doc2vec.py
+++ use_doc2vec.py
@@ -4,6 +4,7 @@ from SVM import SVM
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
 import numpy as np
+import random
 
 # process_all_words('train-pos.txt', 'train-neg.txt', 'test-pos.txt', 'test-neg.txt', 'labeled-data.csv')
 # shuffle('labeled-data.csv', 'shuffled-labeled-data.csv')
@@ -19,11 +20,53 @@ train_inputs, train_labels, test_inputs, test_labels = create_train_test_data('/
                                                                               'shuffled-labeled-data.csv', test_size=0.1)
 '''
 
-# PV-DBOW model
-D2V = doc2vec(optimize='GradDes', learning_rate=0.025, loss_type='sampled_softmax_loss', log="doc2vecV2_log.pickle", model='pv-dbow', n_skip=2, skip_window=2, path="/doc2vecV2_model/")
-D2V.fit(n_epoch=21)
+total_acc = 0.0
+n_train = 10
 
-train_inputs, train_labels, test_inputs, test_labels = create_train_test_data('/doc2vecV2_model/doc_embeddings.pickle', 'shuffled-labeled-data.csv', test_size=0.1)
+for _ in range(n_train):
+# PV-DBOW model
+    D2V = doc2vec(optimize='GradDes', learning_rate=0.025, loss_type='sampled_softmax_loss', log="doc2vecV2_log.pickle", model='pv-dbow', n_skip=2, skip_window=2, path="/doc2vecV2_model/")
+    D2V.fit(n_epoch=21)
+
+    train_inputs, train_labels, test_inputs, test_labels = create_train_test_data('/doc2vecV2_model/doc_embeddings.pickle', 'shuffled-labeled-data.csv', test_size=0.1)
+
+    # Logistic regression from sklearn
+    clf = LogisticRegression()
+    clf.fit(train_inputs, train_labels)
+
+    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+              intercept_scaling=1, penalty='l2', random_state=None, tol=0.0001)
+
+    print(clf.score(test_inputs, test_labels)) # DM - 25 epochs: ~0.674, DBOW - 20 epochs: ~0.83868
+
+    with open('reviews.txt', 'r') as f:
+
+        docs = list(f)
+        random.shuffle(docs)
+        correct = 0
+
+        reviews = []
+        labels = []
+        for line in docs:
+            label = line.split('::')[0]
+            review = line.split('::')[1]
+            reviews.append(review)
+            labels.append(label)
+        new_doc_vectors = D2V.fit_dbow_new_doc(reviews, n_epoch=10, predict_path='predict/')
+
+        predictions = clf.predict(new_doc_vectors)
+        print(predictions)
+        print(labels)
+
+        for i, p in enumerate(predictions):
+            if p == labels[i]:
+                correct += 1
+
+        accuracy = float(correct/len(labels))
+        print('Accuracy: %.2f' % accuracy)
+        total_acc += accuracy
+
+print('Average accuracy: %.2f' % float(total_acc / n_train))
 
 
 '''
@@ -33,22 +76,6 @@ clf.fit(train_inputs, train_labels)
 confidence = clf.score(test_inputs, test_labels)
 print(confidence) # ~0.65
 '''
-
-
-# Logistic regression from sklearn
-clf = LogisticRegression()
-clf.fit(train_inputs, train_labels)
-
-LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-          intercept_scaling=1, penalty='l2', random_state=None, tol=0.0001)
-
-print(clf.score(test_inputs, test_labels)) # DM - 25 epochs: ~0.674, DBOW - 20 epochs: ~0.83868
-
-with open('reviews.txt', 'r') as f:
-    new_doc_vectors = D2V.fit_dbow_new_doc(f, n_epoch=20, predict_path='predict/')
-    print(new_doc_vectors)
-    prediction = clf.predict(new_doc_vectors)
-    print(prediction)
 
 '''
 # Customized SVM class - too long: ~ 4 hours
